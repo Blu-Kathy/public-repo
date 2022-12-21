2b7d910a574625a143dc0dc3e7bf6a934e4e377e
kathytong <22650938+kathytong@users.noreply.github.com>
2022-12-21 13:57:06 -0800

    Update file.py

--- file.py
+++ file.py
@@ -1,17 +1,6 @@
-password = 'ksbdyfgWEUYFT2q0!'
-password = 'ksbdyfgWEUYFT2q1!'
-password = 'ksbdyfgWEUYFT2q2!'
-password = 'ksbdyfgWEUYFT2q3!'
-password = 'ksbdyfgWEUYFT2q4!'
-password = 'ksbdyfgWEUYFT2q5!'
-password = 'ksbdyfgWEUYFT2q6!'
-password = 'ksbdyfgWEUYFT2q7!'
-password = 'ksbdyfgWEUYFT2q8!'
-password = 'ksbdyfgWEUYFT2q9!'
-password = 'ksbdyfgWEUYFT2qa!'
-password = 'ksbdyfgWEUYFT2qb!'
-password = 'ksbdyfgWEUYFT2qc!'
-password = 'ksbdyfgWEUYFT2qd!'
+
+
+
 password = 'ksbdyfgWEUYFT2qe!'
 password = 'ksbdyfgWEUYFT2qf!'
 password = 'ksbdyfgWEUYFT2qg!'
